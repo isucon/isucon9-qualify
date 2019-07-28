8e82a76e2a3f4f402c2844722ccf03bd091a7869
sota1235 <sota1235@gmail.com>
2019-07-29 01:57:47 +0900

    modify: improve style for sign in page

--- webapp/frontend/src/pages/SignInPage.tsx
+++ webapp/frontend/src/pages/SignInPage.tsx
@@ -1,56 +1,79 @@
 import React from 'react';
 
-import { Avatar, Typography, TextField, Button, Grid } from '@material-ui/core';
+import { Avatar, Typography, TextField, Button, Grid, makeStyles } from '@material-ui/core';
 import { LockOutlined } from '@material-ui/icons';
 import { Link as RouteLink } from 'react-router-dom';
 
+const useStyles = makeStyles(theme => ({
+    paper: {
+        marginTop: theme.spacing(1),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%',
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
 
+const SignInPage: React.FC = () => {
+    const classes = useStyles();
 
-const SignInPage: React.FC = () => (
-    <div>
-        <Avatar>
-            <LockOutlined/>
-        </Avatar>
-        <Typography component="h1" variant="h5">
-            ログインページ
-        </Typography>
-        <form noValidate>
-            <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                id="id"
-                label="ログインID"
-                name="id"
-                autoFocus
-            />
-            <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                id="password"
-                label="パスワード"
-                name="password"
-                type="password"
-                autoComplete="current-password"
-            />
-            <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                color="primary"
-            >
-                ログイン
-            </Button>
-            <Grid container>
-                <Grid item xs>
-                    <RouteLink to="/signup">新規登録はこちら</RouteLink>
+    return (
+        <div className={classes.paper}>
+            <Avatar className={classes.avatar}>
+                <LockOutlined/>
+            </Avatar>
+            <Typography component="h1" variant="h5">
+                ログインページ
+            </Typography>
+            <form className={classes.form} noValidate>
+                <TextField
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="id"
+                    label="ログインID"
+                    name="id"
+                    autoFocus
+                />
+                <TextField
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="password"
+                    label="パスワード"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                />
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    className={classes.submit}
+                >
+                    ログイン
+                </Button>
+                <Grid container>
+                    <Grid item xs>
+                        <RouteLink to="/signup">新規登録はこちら</RouteLink>
+                    </Grid>
                 </Grid>
-            </Grid>
-        </form>
-    </div>
-);
+            </form>
+        </div>
+    );
+};
 
 export { SignInPage }
