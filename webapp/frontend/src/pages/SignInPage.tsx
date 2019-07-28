8d9318bd409041954ba31449274332d847c6de11
sota1235 <sota1235@gmail.com>
2019-07-29 01:36:59 +0900

    modify: add sign in form

--- webapp/frontend/src/pages/SignInPage.tsx
+++ webapp/frontend/src/pages/SignInPage.tsx
@@ -1,7 +1,48 @@
 import React from 'react';
 
+import { Avatar, Typography, TextField, Button } from '@material-ui/core';
+import { LockOutlined } from '@material-ui/icons';
+
 const SignInPage: React.FC = () => (
-    <div>Sign in Page</div>
+    <div>
+        <Avatar>
+            <LockOutlined/>
+        </Avatar>
+        <Typography component="h1" variant="h5">
+            ログインページ
+        </Typography>
+        <form noValidate>
+            <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="id"
+                label="ログインID"
+                name="id"
+                autoFocus
+            />
+            <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="password"
+                label="パスワード"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+            />
+            <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+            >
+                ログイン
+            </Button>
+        </form>
+    </div>
 );
 
 export { SignInPage }
