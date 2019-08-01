a5c9fc66c38426703246a9aaf6c3925d73b20453
sota1235 <sota1235@gmail.com>
2019-08-02 04:29:09 +0900

    add: http client

---
+++ webapp/frontend/src/httpClients/appClient.ts
@@ -0,0 +1,24 @@
+import config from '../config';
+
+/**
+ * HTTP client for main app
+ */
+export default class AppClient {
+    private baseUrl: string = config.apiUrl;
+    private defaultHeaders: HeadersInit = {};
+
+    async get(path: string): Promise<Response> {
+        return await fetch(`${this.baseUrl}${path}`, {
+            method: 'GET',
+            headers: this.defaultHeaders,
+        });
+    }
+
+    async post(path: string): Promise<Response> {
+        return await fetch(`${this.baseUrl}${path}`, {
+            method: 'POST',
+            mode: 'cors',
+            headers: this.defaultHeaders,
+        });
+    }
+}
