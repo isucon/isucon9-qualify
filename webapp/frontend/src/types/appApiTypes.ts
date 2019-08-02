7bba207793769fbcf20db1d4c397a214641d73a6
sota1235 <sota1235@gmail.com>
2019-08-02 17:31:47 +0900

    modify: add type for sell endpoint

--- webapp/frontend/src/types/appApiTypes.ts
+++ webapp/frontend/src/types/appApiTypes.ts
@@ -1,3 +1,7 @@
+interface CsrfRequiredReq {
+    csrf_token: string
+}
+
 /**
  * POST /register
  */
@@ -7,10 +11,23 @@ export interface RegisterReqParams {
     address: string
     password: string
 }
-
 // Response
 export interface RegisterResParams extends Response{
     id: number
     account_name: string
     address: string
+}
+
+/**
+ * POST /sell
+ */
+// Request
+export interface SellReq extends CsrfRequiredReq{
+    name: string,
+    price: number,
+    description: string,
+}
+// Response
+export interface SellRes {
+    id: number,
 }
