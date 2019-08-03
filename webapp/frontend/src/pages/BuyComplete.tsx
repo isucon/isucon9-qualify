0d26eed18b6a802ed0780d8da0f51dbfa04fd66e
sota1235 <sota1235@gmail.com>
2019-08-03 10:08:56 +0900

    modify: add page components

---
+++ webapp/frontend/src/pages/BuyComplete.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BuyCompletePage: React.FC = () => (
+    <div>Item buying Page</div>
+);
+
+export { BuyCompletePage }
---
+++ webapp/frontend/src/pages/ItemBuyPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ItemBuyPage: React.FC = () => (
+    <div>Item buying Page</div>
+);
+
+export { ItemBuyPage }
