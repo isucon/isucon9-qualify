6ea725985d461eea2191905c009c29b1fb93777f
sota1235 <sota1235@gmail.com>
2019-08-03 16:44:43 +0900

    add: error pages

---
+++ webapp/frontend/src/pages/error/InternalServerErrorPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {BasePageComponent} from "../../components/BasePageComponent";
+
+const InternalServerErrorPage: React.FC = () => (
+    <BasePageComponent>
+        Internal Server Error
+    </BasePageComponent>
+);
+
+export default InternalServerErrorPage;
---
+++ webapp/frontend/src/pages/error/NotFoundPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {BasePageComponent} from "../../components/BasePageComponent";
+
+const NotFoundPage: React.FC = () => (
+    <BasePageComponent>
+        404 Not Found
+    </BasePageComponent>
+);
+
+export default NotFoundPage;
