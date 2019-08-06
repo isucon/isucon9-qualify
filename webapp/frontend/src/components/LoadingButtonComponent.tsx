d690750ca4cd134480475620f1896b97d67dca1b
sota1235 <sota1235@gmail.com>
2019-08-06 19:49:04 +0900

    modify: add component for loading button

---
+++ webapp/frontend/src/components/LoadingButtonComponent.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import {Button} from "@material-ui/core";
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+type Props = {
+    onClick: (e: React.MouseEvent) => void
+    buttonName: string
+}
+
+export class LoadingButtonComponent extends React.Component<Props> {
+    constructor(props: Props) {
+        super(props);
+
+        this._onClick = this._onClick.bind(this);
+    }
+
+    _onClick(e: React.MouseEvent) {
+        e.preventDefault();
+
+        this.props.onClick(e);
+    }
+
+    render() {
+        const { loading, buttonName } = this.props;
+
+        return (
+            <React.Fragment>
+                <Button
+                    disabled={loading}
+                    onClick={this._onClick}
+                >
+                    {buttonName}
+                </Button>
+                {loading && <CircularProgress size={24} />}
+            </React.Fragment>
+        );
+    }
+};
