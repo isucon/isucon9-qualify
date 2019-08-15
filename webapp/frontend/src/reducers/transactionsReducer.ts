cd55647d538ef96c3fe860165e072d4ee7cbd21c
sota1235 <sota1235@gmail.com>
2019-08-16 02:22:51 +0900

    modify: impl transactions reducer

--- webapp/frontend/src/reducers/transactionsReducer.ts
+++ webapp/frontend/src/reducers/transactionsReducer.ts
@@ -1,5 +1,9 @@
 import { AnyAction } from 'redux';
 import { TransactionItem } from '../dataObjects/item';
+import {
+  FETCH_TRANSACTIONS_SUCCESS,
+  FetchTransactionsSuccessAction,
+} from '../actions/fetchTransactionsAction';
 
 export interface TransactionsState {
   items: TransactionItem[];
@@ -11,13 +15,15 @@ const initialState: TransactionsState = {
   hasNext: false,
 };
 
-type Actions = AnyAction;
+type Actions = FetchTransactionsSuccessAction | AnyAction;
 
 const transactions = (
   state: TransactionsState = initialState,
   action: Actions,
 ): TransactionsState => {
   switch (action.type) {
+    case FETCH_TRANSACTIONS_SUCCESS:
+      return { ...action.payload };
     default:
       return { ...state };
   }
