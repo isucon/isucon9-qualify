663360ed96321e8aafe14c2f40ad4f4427642293
Masahiro Nagano <kazeburo@gmail.com>
2019-08-16 12:43:59 +0900

    use subtest. only running server. remove number of servers test

--- provisioning/image-sharing-test-files/isucon-instance-checker.pl
+++ provisioning/image-sharing-test-files/isucon-instance-checker.pl
@@ -29,26 +29,29 @@ printf("AccountId: %s\n", $identity->{AccountId});
 # Instances
 my $instaces = cmd_json(qw/ecs DescribeInstances --RegionId ap-northeast-1/);
 my @instaces = @{$instaces->{Instances}->{Instance}};
-cmp_ok(scalar @instaces, '<=' , 3, 'Number of instaces should be 3 or less');
 
 for my $instance (@instaces) {
-    my $disks = cmd_json(qw/ecs DescribeDisks --RegionId ap-northeast-1 --InstanceId/,$instance->{InstanceId});
-    my @disks = @{$disks->{Disks}->{Disk}};
+    next if $instance->{Status} ne "Running";
+    
+    subtest sprintf("InstanceId: %s\n",$instance->{InstanceId}) => sub {
+        my $disks = cmd_json(qw/ecs DescribeDisks --RegionId ap-northeast-1 --InstanceId/,$instance->{InstanceId});
+        my @disks = @{$disks->{Disks}->{Disk}};
 
-    is($instance->{InstanceChargeType},'PostPaid','InstanceChargeType should be PostPaid');
-    is($instance->{ZoneId},'ap-northeast-1a','ZoneId should be ap-northeast-1a');
-    is($instance->{InstanceType},'ecs.sn1ne.large','InstanceType should be ecs.sn1ne.large');
-    is($instance->{Cpu},'2','Cpu should be 2 vCPU');
-    is($instance->{Memory},'4096','Memory should be 4096 MB');
-    is($instance->{InternetChargeType},'PayByTraffic','InternetChargeType should be PayByTraffic');
-    is($instance->{InternetMaxBandwidthOut},'100','InternetMaxBandwidthOut should be 100');
+        is($instance->{InstanceChargeType},'PostPaid','InstanceChargeType should be PostPaid');
+        is($instance->{ZoneId},'ap-northeast-1a','ZoneId should be ap-northeast-1a');
+        is($instance->{InstanceType},'ecs.sn1ne.large','InstanceType should be ecs.sn1ne.large');
+        is($instance->{Cpu},'2','Cpu should be 2 vCPU');
+        is($instance->{Memory},'4096','Memory should be 4096 MB');
+        is($instance->{InternetChargeType},'PayByTraffic','InternetChargeType should be PayByTraffic');
+        is($instance->{InternetMaxBandwidthOut},'100','InternetMaxBandwidthOut should be 100');
 
-    is(scalar @disks, 1, 'number of Disks should be 1');
+        is(scalar @disks, 1, 'number of Disks should be 1');
 
-    for my $disk (@disks) {
-        is($disk->{Type}, 'system', 'Disk Type should be system');
-        is($disk->{Size}, '40', 'Disk Size should be 40 GiB');
-        is($disk->{Category}, 'cloud_efficiency', 'Disk Category should be cloud_efficiency(Ultra Disk)');
+        for my $disk (@disks) {
+            is($disk->{Type}, 'system', 'Disk Type should be system');
+            is($disk->{Size}, '40', 'Disk Size should be 40 GiB');
+            is($disk->{Category}, 'cloud_efficiency', 'Disk Category should be cloud_efficiency(Ultra Disk)');
+        }
     }
 }
 
