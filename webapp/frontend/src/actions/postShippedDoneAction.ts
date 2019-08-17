d2103e2bd7a8a5a84e12fcc0d4b59e46f031eb60
sota1235 <sota1235@gmail.com>
2019-08-17 20:43:44 +0900

    modify: re-fetch item data after updating transaction action succeeded

--- webapp/frontend/src/actions/postShippedDoneAction.ts
+++ webapp/frontend/src/actions/postShippedDoneAction.ts
@@ -3,6 +3,9 @@ import { ThunkAction, ThunkDispatch } from 'redux-thunk';
 import { FormErrorState } from '../reducers/formErrorReducer';
 import { Action, AnyAction } from 'redux';
 import { ShipDoneReq, ShipDoneRes } from '../types/appApiTypes';
+import { push } from 'connected-react-router';
+import { routes } from '../routes/Route';
+import { fetchItemAction } from './fetchItemAction';
 
 export const POST_SHIPPED_DONE_START = 'POST_SHIPPED_DONE_START';
 export const POST_SHIPPED_DONE_SUCCESS = 'POST_SHIPPED_DONE_SUCCESS';
@@ -30,6 +33,7 @@ export function postShippedDoneAction(itemId: number): ThunkResult<void> {
       })
       .then((body: ShipDoneRes) => {
         dispatch(postShippedDoneSuccessAction());
+        dispatch(fetchItemAction(itemId.toString())); // FIXME: 異常系のハンドリングが取引ページ向けでない
       })
       .catch((err: Error) => {
         dispatch(
