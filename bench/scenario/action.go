b9c22a872b4b7a71e5c60c445feb6a76f157a002
catatsuy <catatsuy@catatsuy.org>
2019-08-21 16:54:24 +0900

    [bench] add buyComplete

--- bench/scenario/action.go
+++ bench/scenario/action.go
@@ -2,6 +2,7 @@ package scenario
 
 import (
 	"github.com/isucon/isucon9-qualify/bench/asset"
+	"github.com/isucon/isucon9-qualify/bench/server"
 	"github.com/isucon/isucon9-qualify/bench/session"
 	"github.com/morikuni/failure"
 )
@@ -35,4 +36,55 @@ func LoginedSession(user1 asset.AppUser) (*session.Session, error) {
 	}
 
 	return s1, nil
+}
+
+func buyComplete(s1, s2 *session.Session, targetItemID int64) error {
+	token, err := s2.PaymentCard(CorrectCardNumber, IsucariShopID)
+	if err != nil {
+		return err
+	}
+	_, err = s2.Buy(targetItemID, token)
+	if err != nil {
+		return err
+	}
+
+	reserveID, apath, err := s1.Ship(targetItemID)
+	if err != nil {
+		return err
+	}
+
+	md5Str, err := s1.DownloadQRURL(apath)
+	if err != nil {
+		return err
+	}
+
+	sShipment.ForceSetStatus(reserveID, server.StatusShipping)
+	if !sShipment.CheckQRMD5(reserveID, md5Str) {
+		return failure.New(ErrScenario, failure.Message("QRコードの画像に誤りがあります"))
+	}
+
+	err = s1.ShipDone(targetItemID)
+	if err != nil {
+		return err
+	}
+
+	ok := sShipment.ForceDone(reserveID)
+	if !ok {
+		return failure.New(ErrScenario, failure.Message("QRコードのURLに誤りがあります"))
+	}
+
+	err = s2.Complete(targetItemID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func SetShipment(ss *server.ServerShipment) {
+	sShipment = ss
+}
+
+func SetPayment(sp *server.ServerPayment) {
+	sPayment = sp
 }
