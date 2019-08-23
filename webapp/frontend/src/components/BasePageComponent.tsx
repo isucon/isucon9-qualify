2497b7852db6d0f9270048f2ee53608ed0d403f2
sota1235 <sota1235@gmail.com>
2019-08-24 01:37:48 +0900

    fix: for issue of material-ui

--- webapp/frontend/src/components/BasePageComponent.tsx
+++ webapp/frontend/src/components/BasePageComponent.tsx
@@ -17,7 +17,7 @@ import withStyles from '@material-ui/core/styles/withStyles';
 const themeInstance = createMuiTheme({
   palette: {
     background: {
-      default: 'white',
+      default: '#fff',
     },
   },
 });
