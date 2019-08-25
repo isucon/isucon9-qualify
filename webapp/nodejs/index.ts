389a895824f8c7ad64c171bbfa55d28e5ba1b332
FUJI Goro (gfx) <gfuji@cpan.org>
2019-08-25 23:27:25 +0900

    [nodejs] constants and types

--- webapp/nodejs/index.ts
+++ webapp/nodejs/index.ts
@@ -51,10 +51,123 @@ function TODO() {
   throw new Error("Not yet implemented!");
 }
 
-type ReqInitialize = Readonly<{
+const sessionName = "session_isucari";
+const DefaultPaymentServiceURL = "http://localhost:5555";
+const DefaultShipmentServiceURL = "http://localhost:7000";
+const ItemMinPrice = 100;
+const ItemMaxPrice = 1000000;
+const ItemPriceErrMsg =
+  "商品価格は100ｲｽｺｲﾝ以上、1,000,000ｲｽｺｲﾝ以下にしてください";
+const ItemStatusOnSale = "on_sale";
+const ItemStatusTrading = "trading";
+const ItemStatusSoldOut = "sold_out";
+const ItemStatusStop = "stop";
+const ItemStatusCancel = "cancel";
+const PaymentServiceIsucariAPIKey = "a15400e46c83635eb181-946abb51ff26a868317c";
+const PaymentServiceIsucariShopID = "11";
+const TransactionEvidenceStatusWaitShipping = "wait_shipping";
+const TransactionEvidenceStatusWaitDone = "wait_done";
+const TransactionEvidenceStatusDone = "done";
+const ShippingsStatusInitial = "initial";
+const ShippingsStatusWaitPickup = "wait_pickup";
+const ShippingsStatusShipping = "shipping";
+const ShippingsStatusDone = "done";
+const BumpChargeSeconds = 3;
+const ItemsPerPage = 48;
+const TransactionsPerPage = 10;
+const BcryptCost = 10;
+
+type Config = {
+  name: string;
+  val: string;
+};
+
+type User = {
+  id: number;
+  account_name: string;
+  hashed_password: string;
+  address: string;
+  num_sell_items: number;
+  last_bump: Date;
+  created_at: Date;
+};
+
+type UserSimple = {
+  id: number;
+  account_name: string;
+  num_sell_items: number;
+};
+
+type Item = {
+  id: number;
+  seller_id: number;
+  buyer_id: number;
+  status: string;
+  name: string;
+  price: number;
+  description: string;
+  image_name: string;
+  category_id: number;
+  creaed_at: Date;
+  updated_at: Date;
+};
+
+type ItemSimple = {
+  id: number;
+  seller_id: number;
+  seller: UserSimple;
+  status: string;
+  name: string;
+  price: number;
+  description: string;
+  image_url: string;
+  category_id: number;
+  category: Category;
+  creaed_at: Date;
+};
+
+type ItemDetail = {
+  id: number;
+  seller_id: number;
+  seller: UserSimple;
+  buyer_id: number;
+  buyer: UserSimple;
+  status: string;
+  name: string;
+  price: number;
+  description: string;
+  image_url: string;
+  category_id: number;
+  category: Category;
+  transaction_evidence_id: number;
+  transaction_evidence_status: string;
+  shipping_status: string;
+  created_at: Date;
+};
+
+type TransactionEvidence = {
+  id: number;
+  seller_id: number;
+  buyer_id: string;
+  status: string;
+  item_id: string;
+  item_name: string;
+  item_price: number;
+  item_description: string;
+  item_category_id: number;
+  item_root_category_id: number;
+  created_at: Date;
+  updated_at: Date;
+};
+
+type Shipping = {};
+
+type Category = {};
+
+type ReqInitialize = {
   payment_service_url: string;
   shipment_service_url: string;
-}>;
+};
 
 const fastify = createFastify({
   logger: true
@@ -84,6 +197,8 @@ function buildUriFor<T extends IncomingMessage>(request: FastifyRequest<T>) {
 async function getConnection() {
   return fastify.mysql.getConnection();
 }
+
+// API
 
 fastify.post("/initialize", async (req, reply) => {
   const ri: ReqInitialize = req.body;
@@ -188,7 +303,9 @@ fastify.get("/reports.json", (req, reply) => {
 // Frontend
 
 async function getIndex(_req: any, reply: FastifyReply<ServerResponse>) {
-  const html = await fs.promises.readFile(path.join(__dirname, "public/index.html"));
+  const html = await fs.promises.readFile(
+    path.join(__dirname, "public/index.html")
+  );
   reply.type("text/html").send(html);
 }
 
