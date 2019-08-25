876ecfa0b96d377432770cb629ba1876874145bd
FUJI Goro (gfx) <gfuji@cpan.org>
2019-08-25 14:40:46 +0900

    nodejs's mysql driver does not support mysql8's default auth plugin
    https://stackoverflow.com/questions/50373427/node-js-cant-authenticate-to-mysql-8-0

--- webapp/sql/00_create_database.sql
+++ webapp/sql/00_create_database.sql
@@ -2,9 +2,9 @@ DROP DATABASE IF EXISTS `isucari`;
 CREATE DATABASE `isucari`;
 
 DROP USER IF EXISTS 'isucari'@'localhost';
-CREATE USER 'isucari'@'localhost' IDENTIFIED BY 'isucari';
+CREATE USER 'isucari'@'localhost' IDENTIFIED WITH mysql_native_password BY 'isucari';
 GRANT ALL PRIVILEGES ON `isucari`.* TO 'isucari'@'localhost';
 
 DROP USER IF EXISTS 'isucari'@'%';
-CREATE USER 'isucari'@'%' IDENTIFIED BY 'isucari';
+CREATE USER 'isucari'@'%' IDENTIFIED WITH mysql_native_password BY 'isucari';
 GRANT ALL PRIVILEGES ON `isucari`.* TO 'isucari'@'%';
