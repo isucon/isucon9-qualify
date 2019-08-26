49b71993cf69ebe23a61b803fafddfbfed8595cc
Masahiro Nagano <kazeburo@gmail.com>
2019-08-27 00:56:45 +0900

    use union

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -958,8 +958,12 @@ func getTransactions(w http.ResponseWriter, r *http.Request) {
 	if itemID > 0 && createdAt > 0 {
 		// paging
 		err := tx.Select(&items,
-			"SELECT * FROM `items` WHERE (`seller_id` = ? OR `buyer_id` = ?) AND `created_at` <= ? AND `id` < ? ORDER BY `created_at` DESC, `id` DESC LIMIT ?",
+			"(SELECT * FROM `items` WHERE `seller_id` = ? AND `created_at` <= ? AND `id` < ?) "+
+				"UNION (SELECT * FROM `items` WHERE `buyer_id` = ? AND `created_at` <= ? AND `id` < ?) "+
+				"ORDER BY `created_at` DESC, `id` DESC LIMIT ?",
 			user.ID,
+			time.Unix(createdAt, 0),
+			itemID,
 			user.ID,
 			time.Unix(createdAt, 0),
 			itemID,
@@ -974,7 +978,9 @@ func getTransactions(w http.ResponseWriter, r *http.Request) {
 	} else {
 		// 1st page
 		err := tx.Select(&items,
-			"SELECT * FROM `items` WHERE (`seller_id` = ? OR `buyer_id` = ?) ORDER BY `created_at` DESC, `id` DESC LIMIT ?",
+			"SELECT * FROM `items` WHERE `seller_id` = ? "+
+				"UNION (SELECT * FROM `items` WHERE `buyer_id` = ?) "+
+				" ORDER BY `created_at` DESC, `id` DESC LIMIT ?",
 			user.ID,
 			user.ID,
 			TransactionsPerPage+1,
