1aaa562da7a0790978cf172c431e55e6ffafa0bf
sota1235 <sota1235@gmail.com>
2019-08-29 02:31:02 +0900

    modify: improve style of image uploda component

--- webapp/frontend/src/components/ItemImageUploadComponent.tsx
+++ webapp/frontend/src/components/ItemImageUploadComponent.tsx
@@ -4,6 +4,7 @@ import Avatar from '@material-ui/core/Avatar/Avatar';
 import { Camera } from '@material-ui/icons';
 import Typography from '@material-ui/core/Typography/Typography';
 import { Theme } from '@material-ui/core/styles/createMuiTheme';
+import CloudUpload from '@material-ui/icons/CloudUpload';
 import withStyles, {
   WithStyles,
   StyleRules,
@@ -14,8 +15,26 @@ import Button from '@material-ui/core/Button/Button';
 
 const styles = (theme: Theme): StyleRules =>
   createStyles({
+    imagePreviewSpace: {
+      display: 'flex',
+      justifyContent: 'center',
+      flexDirection: 'column',
+      alignItems: 'center',
+      height: '300px',
+    },
+    imagePreviewText: {
+      margin: theme.spacing(2, 0),
+    },
+    imagePreview: {
+      width: 'auto',
+      height: '100%',
+      maxHeight: '300px',
+    },
     upload: {
       display: 'none',
+    },
+    icon: {
+      marginLeft: theme.spacing(1),
     },
     avatar: {
       margin: theme.spacing(1),
@@ -25,20 +44,17 @@ const styles = (theme: Theme): StyleRules =>
     },
   });
 
-interface ItemImageUploadComponentProps extends WithStyles<typeof styles> {
+interface Props extends WithStyles<typeof styles> {
   onImageChange: (image: Blob) => void;
 }
 
-interface ItemImageUploadComponentState {
+interface State {
   file?: File;
   imagePreviewUrl: string;
 }
 
-class ItemImageUploadComponent extends React.Component<
-  ItemImageUploadComponentProps,
-  ItemImageUploadComponentState
-> {
-  constructor(props: ItemImageUploadComponentProps) {
+class ItemImageUploadComponent extends React.Component<Props, State> {
+  constructor(props: Props) {
     super(props);
 
     this.state = {
@@ -79,14 +95,22 @@ class ItemImageUploadComponent extends React.Component<
     let imagePreview = null;
 
     if (imagePreviewUrl) {
-      imagePreview = <img alt="プレビュー" src={imagePreviewUrl} />;
+      imagePreview = (
+        <img
+          className={classes.imagePreview}
+          alt="プレビュー"
+          src={imagePreviewUrl}
+        />
+      );
     } else {
       imagePreview = (
-        <Paper>
+        <Paper className={classes.imagePreviewSpace}>
           <Avatar className={classes.avatar}>
             <Camera />
           </Avatar>
-          <Typography>商品画像</Typography>
+          <Typography className={classes.imagePreviewText} variant="h6">
+            商品画像
+          </Typography>
         </Paper>
       );
     }
@@ -112,10 +136,12 @@ class ItemImageUploadComponent extends React.Component<
           <label htmlFor="outlined-button-file">
             <Button
               variant="outlined"
+              color="default"
               component="span"
               className={classes.button}
             >
               Upload
+              <CloudUpload className={classes.icon} />
             </Button>
           </label>
         </Grid>
