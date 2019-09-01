829589f05315353407526e829126c40bdca64a4a
sota1235 <sota1235@gmail.com>
2019-09-01 20:35:45 +0900

    modify: build assets

--- webapp/public/precache-manifest.f437098649469393969a9e2fa9dce448.js
+++ webapp/public/precache-manifest.f437098649469393969a9e2fa9dce448.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4ecceff56d30f7db3a866cea7a76e376",
+    "revision": "2ca883cbddda3e55055c23f234a8ecdf",
     "url": "/index.html"
   },
   {
-    "revision": "ffc5233259c8a22b348e",
+    "revision": "e6e73cdc6fd431eae8cd",
     "url": "/static/css/main.19393e92.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.535f4c0f.chunk.js"
   },
   {
-    "revision": "ffc5233259c8a22b348e",
-    "url": "/static/js/main.70c385a9.chunk.js"
+    "revision": "e6e73cdc6fd431eae8cd",
+    "url": "/static/js/main.95d07f0b.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
