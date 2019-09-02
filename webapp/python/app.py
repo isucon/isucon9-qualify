a0f0021568cff6e22590e9baa9de046df61d148f
Tatsuhiko Kubo <cubicdaiya@gmail.com>
2019-09-02 21:21:59 +0900

    python: fixed TypeError.
    os.getenv() returns string.

--- webapp/python/app.py
+++ webapp/python/app.py
@@ -69,7 +69,7 @@ def dbh():
 
     flask.g.db = MySQLdb.connect(
         host=os.getenv('MYSQL_HOST', '127.0.0.1'),
-        port=os.getenv('MYSQL_PORT', 3306),
+        port=int(os.getenv('MYSQL_PORT', 3306)),
         user=os.getenv('MYSQL_USER', 'isucari'),
         password=os.getenv('MYSQL_PASS', 'isucari'),
         db=os.getenv('MYSQL_DBNAME', 'isucari'),
