0ae33cb3ca7de08776588f051e8caadff3c46c48
sota1235 <sota1235@gmail.com>
2019-09-03 00:10:01 +0900

    modify: build assets

--- webapp/public/service-worker.js
+++ webapp/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fe1fd537c71f179ee9ef98b9d69762a3.js"
+  "/precache-manifest.f3f96c818d19b9de063ebc400fa65d08.js"
 );
 
 self.addEventListener('message', (event) => {
