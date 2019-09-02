bcb9679be0496893a310d284352f8b8ad89be461
Yamagishi Kazutoshi <ykzts@desire.sh>
2019-09-02 17:56:43 +0900

    fix(ruby): /reports.json のレスポンスから時刻を取り除く

--- webapp/ruby/lib/isucari/web.rb
+++ webapp/ruby/lib/isucari/web.rb
@@ -1190,8 +1190,23 @@ module Isucari
     # getReports
     get '/reports.json' do
       transaction_evidences = db.xquery('SELECT * FROM `transaction_evidences` WHERE `id` > 15007')
+      
+      response = transaction_evidences.map do |transaction_evidence|
+        {
+          'id' => transaction_evidence['id'],
+          'seller_id' => transaction_evidence['seller_id'],
+          'buyer_id' => transaction_evidence['buyer_id'],
+          'status' => transaction_evidence['status'],
+          'item_id' => transaction_evidence['item_id'],
+          'item_name' => transaction_evidence['item_name'],
+          'item_price' => transaction_evidence['item_price'],
+          'item_description' => transaction_evidence['item_description'],
+          'item_category_id' => transaction_evidence['item_category_id'],
+          'item_root_category_id' => transaction_evidence['item_root_category_id']
+        }
+      end
 
-      transaction_evidences.to_a.to_json
+      response.to_json
     end
 
     # Frontend
