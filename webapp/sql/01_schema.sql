13e9568d8f9d72719277e3e2b29a3eeb440c6b27
IzumiSy <beetle-noise@gmx.com>
2020-06-07 18:17:40 +0900

    itemsのcreated_atにインデクスを追加

--- webapp/sql/01_schema.sql
+++ webapp/sql/01_schema.sql
@@ -31,6 +31,7 @@ CREATE TABLE `items` (
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   INDEX idx_category_id (`category_id`)
+  INDEX idx_created_at (`created_at`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4;
 
 DROP TABLE IF EXISTS `transaction_evidences`;
