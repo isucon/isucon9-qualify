452e6ce5f342200b0de316b2958eb16e4589976d
hikaru.sato <hikaru.sato@coconala.com>
2020-08-09 15:38:29 +0900

    itemsにcreated_at のindex追加

--- webapp/sql/01_schema.sql
+++ webapp/sql/01_schema.sql
@@ -30,7 +30,8 @@ CREATE TABLE `items` (
   `category_id` int unsigned NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  INDEX idx_category_id (`category_id`)
+  INDEX idx_category_id (`category_id`),
+  INDEX idx_created_at (`created_at`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4;
 
 DROP TABLE IF EXISTS `transaction_evidences`;
