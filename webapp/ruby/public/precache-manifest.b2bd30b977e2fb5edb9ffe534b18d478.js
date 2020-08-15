a2bef253906bb940834fc8b2fd909a39dde76365
sainu <saino.katsutoshi@moneyforward.co.jp>
2020-08-15 14:04:14 +0900

    Copy webapp/public to webapp/ruby/public

---
+++ webapp/ruby/public/precache-manifest.b2bd30b977e2fb5edb9ffe534b18d478.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcbc5cb038cf9bc853f164a4822931e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f448921f8b046650637",
+    "url": "/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "9f520e8bea2e3943530b",
+    "url": "/static/js/2.ff6e1067.chunk.js"
+  },
+  {
+    "revision": "0f448921f8b046650637",
+    "url": "/static/js/main.babc3d4d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
