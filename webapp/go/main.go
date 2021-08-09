3763241e7ea19160ceebf507c9960a737faaf34b
yuyafukuchi <yuya3542@yahoo.co.jp>
2021-08-09 18:28:44 +0900

    Fix: N+1 in GET /new_items.json endpoint

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -318,7 +318,6 @@ func main() {
 		log.Fatalf("failed to connect to DB: %s.", err.Error())
 	}
 	defer dbx.Close()
-
 	mux := goji.NewMux()
 
 	// API
@@ -557,10 +556,50 @@ func getNewItems(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
+	uniItemIDs := make(map[int64]struct{})
+	var sellerIDs []interface{}
+
+	for _, item := range items {
+		id := item.SellerID
+		if _, ok := uniItemIDs[id]; !ok {
+			uniItemIDs[id] = struct{}{}
+			sellerIDs = append(sellerIDs, id)
+		}
+	}
+
+	var userSimples map[int64]UserSimple
+
+	if len(sellerIDs) > 0 {
+		query := "SELECT * FROM users WHERE id IN (?)"
+		query, args, err := sqlx.In(query, sellerIDs)
+		if err != nil {
+			log.Print(err)
+			outputErrorMsg(w, http.StatusInternalServerError, "db error")
+			return
+		}
+
+		var u []User
+		err = dbx.Select(&u, query, args...)
+		if err != nil {
+			log.Print(err)
+			outputErrorMsg(w, http.StatusInternalServerError, "db error")
+			return
+		}
+
+		userSimples = make(map[int64]UserSimple, len(u))
+		for _, usr := range u {
+			userSimples[usr.ID] = UserSimple{
+				ID:           usr.ID,
+				AccountName:  usr.AccountName,
+				NumSellItems: usr.NumSellItems,
+			}
+		}
+	}
+
 	itemSimples := []ItemSimple{}
 	for _, item := range items {
-		seller, err := getUserSimpleByID(dbx, item.SellerID)
-		if err != nil {
+		seller, ok := userSimples[item.SellerID]
+		if !ok {
 			outputErrorMsg(w, http.StatusNotFound, "seller not found")
 			return
 		}
