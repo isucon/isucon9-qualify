76a4dda4d842e894d09b1ef04febbc82ec36781b
catatsuy <catatsuy@catatsuy.org>
2024-01-14 10:19:58 +0900

    Update payment and shipment service to use configurable ports

--- cmd/shipment/main.go
+++ cmd/shipment/main.go
@@ -16,14 +16,16 @@ func main() {
 	flags.SetOutput(os.Stderr)
 
 	dataDir := ""
+	port := 0
 
 	flags.StringVar(&dataDir, "data-dir", "initial-data", "data directory")
+	flags.IntVar(&port, "port", 7001, "shipment service port")
 	err := flags.Parse(os.Args[1:])
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	liShipment, err := net.ListenTCP("tcp", &net.TCPAddr{Port: 7000})
+	liShipment, err := net.ListenTCP("tcp", &net.TCPAddr{Port: port})
 	if err != nil {
 		log.Fatal(err)
 	}
